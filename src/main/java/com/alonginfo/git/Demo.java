8ce256ce91c73f0a51a5ef4caadd6afbdfb7bb18
rayjp666 <13718193729@163.com>
2018-04-12 10:54:40 +0800

    create project gitdemo

---
+++ src/main/java/com/alonginfo/git/Demo.java
@@ -0,0 +1,10 @@
+package com.alonginfo.git;
+
+/**
+ * Created by rayjp on 2018/4/12 0012.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
