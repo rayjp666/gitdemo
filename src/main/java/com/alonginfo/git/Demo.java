511a2d0b32040ca9a160e5781cce1665871a8c1e
rayjp666 <13718193729@163.com>
2018-04-13 16:22:50 +0800

    update println

--- src/main/java/com/alonginfo/git/Demo.java
+++ src/main/java/com/alonginfo/git/Demo.java
@@ -6,5 +6,6 @@ package com.alonginfo.git;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("git update");
     }
 }
