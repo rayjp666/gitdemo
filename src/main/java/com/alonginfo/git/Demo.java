3c72b6cb6a902ce9d8c6bc54037b1a5c516c9abe
rayjp666 <35122039+rayjp666@users.noreply.github.com>
2018-04-13 16:26:32 +0800

    Update Demo.java
    remote update

--- src/main/java/com/alonginfo/git/Demo.java
+++ src/main/java/com/alonginfo/git/Demo.java
@@ -7,5 +7,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("git update");
+        System.out.println("git remote update");
     }
 }
